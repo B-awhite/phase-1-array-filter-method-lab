67be59b41753ce899c65662c21ea1a7ea7d5566c
Bria White <bria.awhite@gmail.com>
2022-04-15 18:40:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+// function findMatching(collection, array) {
+//     const newCollection = []
+//     for (const driver of collection) {
+//         if (driver === array) {
+//             newCollection.push(driver).toLowerCase
+//         }
+//     }
+//       return newCollection
+// }
+
+function findMatching(array, name) {
+    return array.filter((driver) => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(array, letter) {
+    return array.filter((driver) => driver.startsWith(letter))
+}
+
+function matchName(array, name) {
+    return array.filter((driver) => driver.name === name)
+}
